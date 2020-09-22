f1930c3b2f726b67b731d2fed8a9ef5b22a55aff
Gual Escalante <gual23@gmail.com>
2020-09-21 20:16:34 -0600

    Fixed wrong type on db migration

--- db/migrate/20200922013653_create_recipes.rb
+++ db/migrate/20200922013653_create_recipes.rb
@@ -2,8 +2,8 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.string :name, null: false
-      t.int :coffee, null: false
-      t.int :water, null: false
+      t.integer :coffee, null: false
+      t.integer :water, null: false
       t.text :instructions
 
       t.timestamps
