d7b8bd096e261021c4d462ddaf5cc104a0716584
Gual Escalante <gual23@gmail.com>
2020-09-21 21:52:30 -0600

    Added delete recipe method

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -7,6 +7,7 @@ class Recipe extends React.Component {
     this.state = { recipe: { } };
 
     this.addHtmlEntities = this.addHtmlEntities.bind(this);
+    this.deleteRecipe = this.deleteRecipe.bind(this);
   }
 
   componentDidMount() {
@@ -35,6 +36,32 @@ class Recipe extends React.Component {
       .replace(/&gt;/g, ">");
   }
 
+  deleteRecipe() {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+    const url = `/api/v1/destroy/${id}`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(() => this.props.history.push("/recipes"))
+      .catch(error => console.log(error.message));
+  }
+
   render() {
     const { recipe } = this.state;
 
@@ -61,7 +88,7 @@ class Recipe extends React.Component {
               </ul>
             </div>
             <div className="col-sm-12 col-lg-2">
-              <button type="button" className="btn btn-danger">
+              <button type="button" className="btn btn-danger" onClick={this.deleteRecipe}>
                 Delete Recipe
               </button>
             </div>
