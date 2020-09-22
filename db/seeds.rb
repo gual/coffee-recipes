c422075ceef0dc15172354f9c0e29e17737a27c6
Gual Escalante <gual23@gmail.com>
2020-09-21 20:16:06 -0600

    Added test recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+9.times do |i|
+  Recipe.create(
+    name: "Recipe #{i + 1}",
+    coffee:5 + Random.rand(21),
+    water: 16 + Random.rand(31)
+  )
+end
